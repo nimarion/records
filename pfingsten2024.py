b257f178e3da238a58771e40b9c53804e50e96e4
Niklas Marion <niklas.marion@gmx.de>
2024-02-25 20:25:09 +0100

    run filter for pfingsten

---
+++ pfingsten2024.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_csv('taf.csv')
+
+women = ["100", "400", "800", "1500", "400H", "SPE", "KUG", "HOC"]
+
+men = ["100", "400", "800", "1500", "400H",
+       "SPE", "KUG", "HOC", "3KH", "STA", "5K0"]
+
+men_df = df[(df['event'].isin(men)) & (df['class'] == 'M')]
+women_df = df[(df['event'].isin(women)) & (df['class'] == 'W')]
+
+df = pd.concat([men_df, women_df])
+
+df.to_excel('pfingsten2024.xlsx', index=False)
