565b3516eaf9ec2ce5d796f76d2d20c8f5322b38
Niklas Marion <niklas.marion@gmx.de>
2024-03-15 22:04:57 +0100

    add filter script

---
+++ filter.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import argparse
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '--input', help='Input taf file', required=True)
+    argparser.add_argument(
+        '--output', help='Output taf file', required=True)
+    argparser.add_argument("--disciplines", help="Disciplines filter file", required=True)
+    args = argparser.parse_args()
+
+    input_data = pd.read_csv(args.input).astype(str)
+    disciplines = pd.read_csv(args.disciplines).astype(str)
+
+    input_data['discipline_sex'] = input_data['discipline'] + '_' + input_data['sex']
+    disciplines['discipline_sex'] = disciplines['discipline'] + '_' + disciplines['sex']
+
+    unique_discipline_sex = disciplines['discipline_sex'].unique()
+    filtered_input = input_data[input_data['discipline_sex'].isin(unique_discipline_sex)]
+
+    filtered_input = filtered_input.drop(columns=['discipline_sex'])
+
+    filtered_input = filtered_input.fillna("")
+    filtered_input = filtered_input.replace("nan", "")
+
+    filtered_input.to_csv(args.output, index=False)
